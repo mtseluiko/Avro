cbb0eaf86d8d82892bbc47b4c1e63d47767a763c
mtseluiko <mikhail.tseluiko@binary-studio.com>
2019-09-09 11:43:12 +0300

    JSONSchema RE: add handling for number/integer types

--- reverse_engineering/helpers/adaptJsonSchema.js
+++ reverse_engineering/helpers/adaptJsonSchema.js
@@ -1,4 +1,5 @@
 const mapJsonSchema = require('./mapJsonSchema');
+const _ = require('lodash');
 
 const handleDate = field => {
 	return Object.assign({}, field, {
@@ -24,23 +25,82 @@ const handleDateTime = field => {
 	});
 };
 
+const handleNumber = field => {
+	return Object.assign({}, field, {
+		type: 'bytes',
+		logicalType: 'decimal'
+	});
+};
+
+const handleInt = field => {
+	return Object.assign({}, field, {
+		type: 'number',
+		mode: 'int'
+	});
+};
+
+const handleStringFormat = field => {
+	switch(field.format) {
+		case 'date':
+			return handleDate(field);
+		case 'time':
+			return handleTime(field);
+		case 'date-time':
+			return handleDateTime(field);
+		default:
+			return field;
+	};
+};
+
+const adaptMultiple = field => {
+	const { fieldData, types } = field.type.reduce(({ fieldData, types }, type) => {
+		const typeField = Object.assign({}, fieldData, { type });
+		const updatedData = adaptType(typeField);
+		const updatedTypes = types.map(initialType => {
+			if (initialType === type) {
+				return updatedData.type;
+			}
+			return initialType;
+		});
+
+		return {
+			fieldData: updatedData,
+			types: _.uniq(updatedTypes)
+		};
+	}, { fieldData: field, types: field.type });
+
+	if (types.length === 1) {
+		return fieldData;
+	}
+
+	return Object.assign({}, fieldData, {type: types});
+};
+
+const adaptType = field => {
+	const type = field.type;
+
+	if (_.isArray(type)) {
+		return adaptMultiple(field);
+	}
+
+	if (type === 'string') {
+		return handleStringFormat(field);
+	}
+
+	if (type === 'number') {
+		return handleNumber(field);
+	}
+
+	if (type === 'integer' || type === 'int') {
+		return handleInt(field);
+	}
+
+	return field;
+};
 
 const adaptJsonSchema = jsonSchema => {
 	return mapJsonSchema(jsonSchema, jsonSchemaItem => {
-		if (jsonSchemaItem.type !== 'string') {
-			return jsonSchemaItem;
-		}
-
-		switch(jsonSchemaItem.format) {
-			case 'date': 
-				return handleDate(jsonSchemaItem);
-			case 'time': 
-				return handleTime(jsonSchemaItem);
-			case 'date-time': 
-				return handleDateTime(jsonSchemaItem);
-			default:
-				return jsonSchemaItem;
-		};
+		return adaptType(jsonSchemaItem);
 	});
 };
 
